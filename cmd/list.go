bd1a3b5080c4007881f462b0c2b5ce387215aec6
robert <rsandova@redhat.com>
2022-02-01 15:35:04 -0600

    working commit

--- cmd/list.go
+++ cmd/list.go
@@ -18,6 +18,7 @@ package cmd
 import (
 	"fmt"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 	"io/ioutil"
 	"path/filepath"
 	"strings"
@@ -30,8 +31,12 @@ var listCmd = &cobra.Command{
 	Long:  pluginCmdLongText,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("list called")
+		run()
 	},
 }
+
+var ValidPluginFilenamePrefixes = []string{"oups"}
+var configs = make(map[string]viper.Viper)
 
 func init() {
 	pluginCmd.AddCommand(listCmd)
@@ -40,91 +45,38 @@ func init() {
 func run() {
 	pluginsFound := false
 	isFirstFile := true
-	pluginErrors := []error{}
-	pluginWarnings := 0
+	dir := "/home/robert/.local/share/oups"
 
-	for _, dir := range uniquePathsList(o.PluginPaths) {
-		if len(strings.TrimSpace(dir)) == 0 {
+	files, err := ioutil.ReadDir(dir)
+	fmt.Println(err)
+
+	for _, f := range files {
+		if f.IsDir() {
+			continue
+		}
+		if !hasValidPrefix(f.Name(), ValidPluginFilenamePrefixes) {
 			continue
 		}
 
-		files, err := ioutil.ReadDir(dir)
-		if err != nil {
-			if _, ok := err.(*os.PathError); ok {
-				fmt.Fprintf(o.ErrOut, "Unable to read directory %q from your PATH: %v. Skipping...\n", dir, err)
-				continue
-			}
-
-			pluginErrors = append(pluginErrors, fmt.Errorf("error: unable to read directory %q in your PATH: %v", dir, err))
-			continue
+		if isFirstFile {
+			fmt.Println("The following compatible plugins are available:\n\n")
+			pluginsFound = true
+			isFirstFile = false
 		}
 
-		for _, f := range files {
-			if f.IsDir() {
-				continue
-			}
-			if !hasValidPrefix(f.Name(), ValidPluginFilenamePrefixes) {
-				continue
-			}
+		pluginPath := f.Name()
+		//		if !o.NameOnly {
+		pluginPath = filepath.Join(dir, pluginPath)
+		//		}
 
-			if isFirstFile {
-				fmt.Fprintf(o.Out, "The following compatible plugins are available:\n\n")
-				pluginsFound = true
-				isFirstFile = false
-			}
-
-			pluginPath := f.Name()
-			if !o.NameOnly {
-				pluginPath = filepath.Join(dir, pluginPath)
-			}
-
-			fmt.Fprintf(o.Out, "%s\n", pluginPath)
-			if errs := o.Verifier.Verify(filepath.Join(dir, f.Name())); len(errs) != 0 {
-				for _, err := range errs {
-					fmt.Fprintf(o.ErrOut, "  - %s\n", err)
-					pluginWarnings++
-				}
-			}
-		}
+		fmt.Println(pluginPath)
 	}
 
 	if !pluginsFound {
-		pluginErrors = append(pluginErrors, fmt.Errorf("error: unable to find any kubectl plugins in your PATH"))
+		fmt.Println("no plugins found")
 	}
-
-	if pluginWarnings > 0 {
-		if pluginWarnings == 1 {
-			pluginErrors = append(pluginErrors, fmt.Errorf("error: one plugin warning was found"))
-		} else {
-			pluginErrors = append(pluginErrors, fmt.Errorf("error: %v plugin warnings were found", pluginWarnings))
-		}
-	}
-	if len(pluginErrors) > 0 {
-		errs := bytes.NewBuffer(nil)
-		for _, e := range pluginErrors {
-			fmt.Fprintln(errs, e)
-		}
-		return fmt.Errorf("%s", errs.String())
-	}
-
-	return nil
+	readConfig()
 }
-
-// uniquePathsList deduplicates a given slice of strings without
-// sorting or otherwise altering its order in any way.
-func uniquePathsList(paths []string) []string {
-	seen := map[string]bool{}
-	newPaths := []string{}
-	for _, p := range paths {
-		if seen[p] {
-			continue
-		}
-		seen[p] = true
-		newPaths = append(newPaths, p)
-	}
-	return newPaths
-}
-
 func hasValidPrefix(filepath string, validPrefixes []string) bool {
 	for _, prefix := range validPrefixes {
 		if !strings.HasPrefix(filepath, prefix+"-") {
@@ -133,4 +85,16 @@ func hasValidPrefix(filepath string, validPrefixes []string) bool {
 		return true
 	}
 	return false
+}
+func readConfig() {
+
+	// TODO: this needs to be generic to read in all configs
+	loadbalancer := viper.New()
+	loadbalancer.AddConfigPath("/home/robert/.local/share/oups/")
+	loadbalancer.SetConfigName("oups-loadbalancer")
+	loadbalancer.SetConfigType("yaml")
+	loadbalancer.ReadInConfig()
+	fmt.Println(loadbalancer.Get("image"))
+	configs["loadbalancer"] = *loadbalancer
+
 }
